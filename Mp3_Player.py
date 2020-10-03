33173c8058a50969ced4bb7f843a27814f437218
pankaj pandey <49112512+pankz-104@users.noreply.github.com>
2020-10-03 23:34:32 +0530

    Update Mp3_Player.py

--- Mp3_Player.py
+++ Mp3_Player.py
@@ -1,3 +1,4 @@
+# importing required functionalities
 from pygame import mixer
 from tkinter import filedialog
 from tkinter import * 
@@ -5,29 +6,31 @@ from tkinter import *
 
 class MusicPlayer:
     def __init__(self, root):
-        root.geometry('400x300')
-        root.title('Music Player')
-        Load = Button(root, text='Load', width=10, font=('Times',10), command = self.load)
+        root.geometry('400x300')    # window size 
+        root.title('Music Player')      # window titel 
+        Load = Button(root, text='Load', width=10, font=('Times',10), command = self.load) # Buttons 
         Play = Button(root, text='Play', width=10, font=('Times',10), command = self.play)
         Pause = Button(root, text='Pause', width=10, font=('Times',10), command = self.pause)
         Stop = Button(root, text='Stop', width=10, font=('Times',10), command = self.stop)
-        Load.place(x = 0,y = 20)
+        exit = Button(root, text='Exit', width=10, font=('Times',10), command = self.exit())
+        Load.place(x = 0,y = 20)     # positioning buttons 
         Play.place(x = 100, y = 20)
         Pause.place(x = 200, y = 20)
         Stop.place(x = 300, y = 20)
+        Exit.place(x = 100, y = -20)
         self.music_file = False
         self.playing_state = False
 
-    def load(self):
+    def load(self):     # opening from file exxplorer and loading music file 
         self.music_file = filedialog.askopenfilename()
 
-    def play(self):
+    def play(self):     # playing music file 
         if self.music_file:
             mixer.init()
             mixer.music.load(self.music_file)
             mixer.music.play()
 
-    def pause(self):
+    def pause(self):    # pause and unpause the music file 
         if not self.playing_state:
             mixer.music.pause()
             self.playing_state = True
@@ -35,7 +38,7 @@ class MusicPlayer:
             mixer.music.unpause()
             self.playing_state = False
 
-    def stop(self):
+    def stop(self):     # stoping music file 
             mixer.music.stop()
 
 
