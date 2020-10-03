16aea1cd6df9b8abc266b1943356fba064fcba77
pankaj pandey <49112512+pankz-104@users.noreply.github.com>
2020-10-03 23:11:53 +0530

    Create Mp3_Player.py

---
+++ Mp3_Player.py
@@ -0,0 +1,44 @@
+from pygame import mixer
+from tkinter import filedialog
+from tkinter import * 
+
+
+class MusicPlayer:
+    def __init__(self, root):
+        root.geometry('400x300')
+        root.title('Music Player')
+        Load = Button(root, text='Load', width=10, font=('Times',10), command = self.load)
+        Play = Button(root, text='Play', width=10, font=('Times',10), command = self.play)
+        Pause = Button(root, text='Pause', width=10, font=('Times',10), command = self.pause)
+        Stop = Button(root, text='Stop', width=10, font=('Times',10), command = self.stop)
+        Load.place(x = 0,y = 20)
+        Play.place(x = 100, y = 20)
+        Pause.place(x = 200, y = 20)
+        Stop.place(x = 300, y = 20)
+        self.music_file = False
+        self.playing_state = False
+
+    def load(self):
+        self.music_file = filedialog.askopenfilename()
+
+    def play(self):
+        if self.music_file:
+            mixer.init()
+            mixer.music.load(self.music_file)
+            mixer.music.play()
+
+    def pause(self):
+        if not self.playing_state:
+            mixer.music.pause()
+            self.playing_state = True
+        else:
+            mixer.music.unpause()
+            self.playing_state = False
+
+    def stop(self):
+            mixer.music.stop()
+
+
+root = Tk()
+mp3_player = MusicPlayer(root)
+root.mainloop()
